0036ee0c589cca3a14bc409d08e07d4fab8d98d7
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2021-04-10 16:56:48 -0600

    Use strict, logic finished, files now saved to tools folder, log messages added
    Signed-off-by: Rudy de Lorenzo <rdelorenzo5@gmail.com>

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
@@ -6,12 +8,42 @@ const fetch = require('node-fetch');
 const stream = require('stream');
 const DOMParser = require('jsdom');
 
-const sessID = "12312edfhjbasd";    // TODO: generate randomly each run
+//TODO: Complete JSON file
+//TODO: Comment code
+//TODO: Wrap in .command and .bat file
+//TODO: Use 10GBit servers
+//TODO: Move to TypeScript
+
+const dirname = "TOOLS"
 const pipeline = util.promisify(stream.pipeline);
+
+let locations;
+try {
+    locations = JSON.parse(fs.readFileSync("locations.json"));
+} catch (err) {
+    console.log("\x1b[31m%s\x1b[0m", "✗ | Couldn't read locations.json, aborting...");
+    console.error(err);
+    return
+}
+
+if (!fs.existsSync(dirname)){
+    fs.mkdirSync(dirname);
+}
+
+let opts = {
+    headers: {
+        Host: "www.guru3d.com",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+    },
+    redirect: "manual"
+};
 
 async function download(url, filename) {
     if (!filename) filename = path.basename(url);    // handles optional filename argument
-    const file = fs.createWriteStream(filename);
+    const file = fs.createWriteStream(path.join(dirname, filename));
 
     let response, body;
 
@@ -26,7 +58,7 @@ async function download(url, filename) {
         return
     }
 
-    let totalsize = parseInt(response.headers.get('content-length'), 10);
+    let totalSize = parseInt(response.headers.get('content-length'), 10);
     let downloaded = 0;             // not in use at the moment
 
     let pb = new ProgressBar(`* | ${filename}\t :percent [:bar] :rate b/s ETA :etas`, {
@@ -34,7 +66,7 @@ async function download(url, filename) {
         head: '>',
         incomplete: ' ',
         width: 30,
-        total: totalsize
+        total: totalSize
     });
 
     response.body.on('data', (chunk) => {
@@ -43,49 +75,68 @@ async function download(url, filename) {
     });
 
     try {
-        const file = fs.createWriteStream(filename);
         await pipeline(body, file);
     } catch (err) {
         file.close();
         fs.unlinkSync(filename); // delete file in case of failure
-        console.log("\x1b[31m%s\x1b[0m", "✗ | Download failed :(");
-        console.error(err);
-        return
+        throw err;
     }
 
     file.close();
-    console.log("\x1b[32m%s\x1b[0m", `✔ | Downloaded ${filename}! (${totalsize})`);
+    return {filename: filename, size: totalSize};
 }
 
-async function getUtil(url) {
+async function getUtil(url, name) {
+    if (!name) name = url;
+
     // find div containing links
+    console.log(`* | Now downloading ${name}...`);
+    let page = await fetch(url, opts);
+    opts.headers.Cookie = newCookie(page.headers);  // set PHPSESSID if a new one was sent
+    page = await page.text();
+    let downloadPage = new DOMParser.JSDOM(page).window.document.body.getElementsByClassName("lower-greek")[0].children[0].href;
+
     // go to download page
+    await fetch(downloadPage, opts)
+
     // go to common download endpoint and intercept 302 location
-    let opts = {
-        headers: {
-            cookie: sessID
-        },
-        redirect: "manual"
-    };
-
     let address, response;
-
     try {
-        response = await fetch("https://www.guru3d.com/index.php?ct=files&action=download&", opts);
+        response = await fetch(`https://www.guru3d.com/index.php?ct=files&action=download&`, opts);
         address = response.headers.get("location");
-        console.log(address);
+        if (!address) throw new Error("Location header was empty, download not attempted...");
     } catch (err) {
         console.error(err);
         return
     }
 
     // download from location address
-    await download(address)
+    try {
+        let {filename, size} = await download(address)
+        console.log("\x1b[32m%s\x1b[0m", `✔ | Downloaded ${filename}! (${size})`);
+    } catch (err) {
+        console.log("\x1b[31m%s\x1b[0m", "✗ | Download failed :(");
+        console.error(err);
+    }
+}
+
+function newCookie(headers) {
+    let setCookie = headers.get("set-cookie");
+    if (setCookie) {
+        let cookieValStart = setCookie.indexOf("PHPSESSID=") + 10;
+        let cookieValEnd = setCookie.indexOf(";", cookieValStart);
+        if (cookieValEnd === -1 || cookieValStart === -1) return opts.headers.Cookie;
+        return `PHPSESSID=${setCookie.substring(cookieValStart, cookieValEnd)}`;
+    } else {
+        return opts.headers.Cookie
+    }
 }
 
 async function main() {
-    await getUtil("");
-    console.log("All Done! :)");
+    for (let key in locations) {
+        if (locations.hasOwnProperty(key)) await getUtil(locations[key], key);
+    }
+    console.log("\x1b[32m%s\x1b[0m", `\n✔ | All Done!\n✔ | You can find the downloaded files in ${path.join(__dirname, dirname)}`);
 }
 
 main()
