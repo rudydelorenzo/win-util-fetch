eb06e79771d75b58c100c85eda9321bade20ad28
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2021-04-12 23:47:22 -0600

    Laid foundation for downloads from multiple sources
    Signed-off-by: Rudy de Lorenzo <rdelorenzo5@gmail.com>

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const DOMParser = require('jsdom');
 //TODO: Wrap in .command and .bat file
 //TODO: Use 10GBit servers
 //TODO: Move to TypeScript
+//TODO: Print total runtime at the end
+//TODO: Extract zip
 
 const dirname = "TOOLS"
 const pipeline = util.promisify(stream.pipeline);
@@ -43,9 +45,8 @@ let opts = {
 
 async function download(url, filename) {
     if (!filename) filename = path.basename(url);    // handles optional filename argument
-    const file = fs.createWriteStream(path.join(dirname, filename));
 
-    let response, body;
+    let file, response, body, headers, knownLength = true;
 
     try {
         response = await fetch(url);
@@ -58,8 +59,17 @@ async function download(url, filename) {
         return
     }
 
-    let totalSize = parseInt(response.headers.get('content-length'), 10);
-    let downloaded = 0;             // not in use at the moment
+    headers = response.headers;
+
+    let headerFilename = getFilenameFromHeaders(headers);
+    filename = (headerFilename === undefined) ? filename : headerFilename;
+    file = fs.createWriteStream(path.join(dirname, filename));
+
+    let contentLength = headers.get('content-length');
+    knownLength = !!contentLength;
+    console.log("\x1b[33m%s\x1b[0m", "⚠ | Will download without progress bar. Tell the server owner to include a content-length header!");
+    let totalSize = (knownLength) ? parseInt(headers.get('content-length'), 10) : 1;
+    let downloaded = 0;
 
     let pb = new ProgressBar(`* | ${filename}\t :percent [:bar] :rate b/s ETA :etas`, {
         complete: '=',
@@ -71,7 +81,9 @@ async function download(url, filename) {
 
     response.body.on('data', (chunk) => {
         downloaded += chunk.length; // not in use at the moment
-        pb.tick(chunk.length);
+        if (knownLength) {
+            pb.tick(chunk.length);
+        }
     });
 
     try {
@@ -82,15 +94,39 @@ async function download(url, filename) {
         throw err;
     }
 
+    if (!knownLength) pb.tick(1);
     file.close();
-    return {filename: filename, size: totalSize};
+
+    return {filename: filename, size: downloaded};
 }
 
-async function getUtil(url, name) {
+async function get(url, name) {
     if (!name) name = url;
 
-    // find div containing links
+    // console sugar
     console.log(`* | Now downloading ${name}...`);
+
+    let address;
+
+    // choose url extraction algorithm  or default to simple download
+    if (url.includes("guru3d")) {
+        address = await getGuru3D(url, name);
+    } else {
+        address = url;
+    }
+
+    // download and print console sugar
+    try {
+        let {filename, size} = await download(address)
+        console.log("\x1b[32m%s\x1b[0m", `✔ | Downloaded ${filename}! (${(size/1000000).toFixed(2)} MB)`);
+    } catch (err) {
+        console.log("\x1b[31m%s\x1b[0m", "✗ | Download failed :(");
+        console.error(err);
+    }
+}
+
+async function getGuru3D(url, name) {
+    // find div containing links
     let page = await fetch(url, opts);
     opts.headers.Cookie = newCookie(page.headers);  // set PHPSESSID if a new one was sent
     page = await page.text();
@@ -104,20 +140,14 @@ async function getUtil(url, name) {
     try {
         response = await fetch(`https://www.guru3d.com/index.php?ct=files&action=download&`, opts);
         address = response.headers.get("location");
-        if (!address) throw new Error("Location header was empty, download not attempted...");
+        if (!address) throw new Error("3D guru location header was empty, returning undefined...");
     } catch (err) {
         console.error(err);
-        return
+        return address;
     }
 
-    // download from location address
-    try {
-        let {filename, size} = await download(address)
-        console.log("\x1b[32m%s\x1b[0m", `✔ | Downloaded ${filename}! (${(size/1000000).toFixed(2)} MB)`);
-    } catch (err) {
-        console.log("\x1b[31m%s\x1b[0m", "✗ | Download failed :(");
-        console.error(err);
-    }
+    return address;
+
 }
 
 function newCookie(headers) {
@@ -125,17 +155,31 @@ function newCookie(headers) {
     if (setCookie) {
         let cookieValStart = setCookie.indexOf("PHPSESSID=") + 10;
         let cookieValEnd = setCookie.indexOf(";", cookieValStart);
-        if (cookieValEnd === -1 || cookieValStart === -1) return opts.headers.Cookie;
+        if (cookieValEnd === -1 || cookieValStart === 9) return opts.headers.Cookie;
         return `PHPSESSID=${setCookie.substring(cookieValStart, cookieValEnd)}`;
     } else {
         return opts.headers.Cookie
     }
 }
 
+function getFilenameFromHeaders(headers) {
+    let filename = undefined;
+    try {
+        let matches = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(headers.get("content-disposition"));
+        if (matches != null && matches[1]) {
+            filename = matches[1].replace(/['"]/g, '');
+        }
+        return filename;
+    } catch (err) {
+        return filename;
+    }
+}
+
 async function main() {
     for (let key in locations) {
-        if (locations.hasOwnProperty(key)) await getUtil(locations[key], key);
+        if (locations.hasOwnProperty(key)) await get(locations[key], key);
     }
+
     console.log("\x1b[32m%s\x1b[0m", `\n✔ | All Done!\n✔ | You can find the downloaded files in ${path.join(__dirname, dirname)}`);
 }
 
